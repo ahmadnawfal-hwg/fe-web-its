a0ae11f87702a5e7f901b3879fbf395be0570415
Ahmad Nawfal <ahmad.nawfal@hwgroup.id>
2025-06-23 13:21:34 +0700

    test: husky with lint-staged

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     class="min-h-screen flex flex-col items-center justify-center bg-gray-50 text-center px-4"
   >
     <h1 class="text-3xl md:text-5xl font-bold text-blue-600 mb-4">
-      Welcome to Minimal Starter Pack
+      Welcome to Minimal Starter Pack + Nuxt 3 + Tailwind CSS
     </h1>
     <p class="text-lg text-gray-600">This is the home page.</p>
   </div>
